053c4dd1045a905b1bfdf8890cd794eb419ad30b
Miguel <maflores17@miners.utep.edu>
2015-07-02 13:13:41 -0600

    Make Pie appear first

--- Scripts/VM_Field.js
+++ Scripts/VM_Field.js
@@ -1,5 +1,19 @@
 ﻿var available = true;
-
+var arrAvail = [
+			true,
+			true,
+			true,
+			true,
+			true,
+			true,
+			true,
+			true,
+			true,
+			true,
+			true,
+			true,
+		];
+		calendarToggle(arrAvail);
 // Overall viewmodel for this screen, along with initial state
 function ScheduleViewModel() {
 	
@@ -27,20 +41,7 @@ function ScheduleViewModel() {
 
 	function myFunction() 
 	{
-		var arrAvail = [
-			true,
-			true,
-			true,
-			true,
-			true,
-			true,
-			true,
-			true,
-			true,
-			true,
-			true,
-			true,
-		];
+		
 		var x;
 				for(var i = 0; i < arrAvail.length; i++)
 				{
@@ -62,7 +63,7 @@ function ScheduleViewModel() {
 					}
 					
 				}
-		calendarToggle(arrAvail)
+		calendarToggle(arrAvail);
 	}
 
 
@@ -91,63 +92,67 @@ function calendarToggle(avail) {
 		for (var j = 0; j < myData.length; j++) {
 		myTotal += (typeof myData[j].divide == 'number') ? myData[j].divide : 0;
 		}
-		return myTotal;
+			return myTotal;
 		}
 
 		function plotData() {
-		var canvas;
-		var ctx;
-		var lastend = -1.6;
-		var myTotal = getTotal();
-		
-		canvas = document.getElementById("canvas");
-		ctx = canvas.getContext("2d");
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-		for (var i = 0; i < myData.length; i++) {
-		
-		
-		/*var ToF = prompt("Please Enter if Available")
-			if(ToF == "false"){
-				availability[i].available = false;
-				
-			}
+			var canvas;
+			var ctx;
+			var lastend = -1.6;
+			var myTotal = getTotal();
 			
-			else{
-				availability[i].available = true;
-			}*/
-		
-		if(availability[i] == true){
-		ctx.fillStyle = "green";
-		
-		}
-		else if(availability[i] == false){
-		ctx.fillStyle = "red";
-		}
-		
-		ctx.beginPath();
-		ctx.moveTo(200,150);
-		ctx.arc(200,150,150,lastend,lastend+
-		  (Math.PI*2*(myData[i].divide/myTotal)),false);
-		ctx.lineTo(200,150);
-		ctx.fill();
-		lastend += Math.PI*2*(myData[i].divide/myTotal);
-		
-		var c=document.getElementById("canvas");
-		var ctx=c.getContext("2d");
-		ctx.beginPath();
-		ctx.fillStyle = "white";
-		ctx.arc(200,150,125,0,2*Math.PI);
-		ctx.stroke();
-		ctx.fill();
-		
+			canvas = document.getElementById("canvas");
+			ctx = canvas.getContext("2d");
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+			
+				
+				for (var i = 0; i < myData.length; i++) {
+				
+				//debugging code
+					
+				
+				//////////////////////////////////////////////end debug
+				
+					if(availability[i] == true){
+						ctx.fillStyle = "green";
+					
+					}
+					else if(availability[i] == false){
+						ctx.fillStyle = "red";
+					}
+					
+				ctx.beginPath();
+				ctx.moveTo(200,150);
+				ctx.arc(200,150,150,lastend,lastend+
+				  (Math.PI*2*(myData[i].divide/myTotal)),false);
+				ctx.lineTo(200,150);
+				ctx.fill();
+				lastend += Math.PI*2*(myData[i].divide/myTotal);
+				
+				var c=document.getElementById("canvas");
+				var ctx=c.getContext("2d");
+				ctx.beginPath();
+				ctx.fillStyle = "white";
+				ctx.arc(200,150,125,0,2*Math.PI);
+				ctx.stroke();
+				ctx.fill();
 
-		
-		
+					}
+					
+					
 		}
-		}
-
+	
 		plotData();
 	}
+	
+		function q(event) {
+    event = event || window.event;
+
+    var canvas = document.getElementById('canvas'),
+        x = event.pageX - canvas.offsetLeft,
+        y = event.pageY - canvas.offsetTop;
+
+    alert(x + ' ' + y);
+}
 
 ko.applyBindings(new ScheduleViewModel());
